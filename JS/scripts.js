482b5fc0a7e214d9f2f8630d0335951055971eb5
jorge <jorluar372@gmail.com>
2022-02-27 15:26:49 -0300

    Se agrega validacion de texto de entrada y enlaces a linkedin y github

--- JS/scripts.js
+++ JS/scripts.js
@@ -4,8 +4,20 @@ document.querySelector('#copy').addEventListener('click', copyText)
 
 
 function encryption() {
-
+    event.preventDefault()
     let text = document.querySelector("#textEncryption").value;
+
+    var errorText = validationIn(text)
+    if (errorText.length > 0) {
+        showerrors(errorText)
+        let textBoxOut = document.querySelector("#resultEncryption")
+        textBoxOut.innerHTML = ""
+        return
+    } else {
+        var ul = document.querySelector("#errorValidation")
+        ul.innerHTML = ""
+    }
+
     let stringText = text.split(' ') // aca separo el texto en un array de palabras
 
     for (let i = 0; i < stringText.length; i++) {
@@ -32,7 +44,36 @@ function showEncryption(text) {
 
 function copyText() {
     let textCopy = document.querySelector("#resultEncryption")
+    var text = document.querySelector("#textEncryption")
     textCopy.select()
     document.execCommand('copy')
     alert("Copied!")
+    textCopy.innerHTML = "";
+    text.innerHTML = "";
+}
+
+function validationIn(text) {
+    var errors = [];
+    const expresionRegular1 = new RegExp('[A-Z]', 'g')
+    const expresionRegular2 = new RegExp('[#,$,!,¡,",%,&,´,á,ó,í,ú,é]', 'g')
+    if (text.length == 0) {
+        errors.push("No ingreso texto a encryptar")
+    }
+    if (expresionRegular1.test(text)) {
+        errors.push("Solo puede ingresar texto en minuscula")
+    }
+    if (expresionRegular2.test(text)) {
+        errors.push("No se admiten caracteres especiales")
+    }
+    return errors
+}
+
+function showerrors(errors) {
+    var ul = document.querySelector("#errorValidation")
+    ul.innerHTML = ""
+    errors.forEach(err => {
+        var li = document.createElement("li")
+        li.textContent = err
+        ul.appendChild(li)
+    });
 }
